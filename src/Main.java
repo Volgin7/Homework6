d36b3a6bdf08a41c01aae00de554668a276f7451
Volgin7 <8612020@gmail.com>
2023-02-27 12:47:32 +0300

    +task7

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         task4();
         task5();
         task6();
+        task7();
     }
 
     public static void task1() {
@@ -46,6 +47,13 @@ public class Main {
     public static void task6() {
         System.out.println("Задача 6");
         for ( int i = 7; i <= 98; i += 7 ) {
+            System.out.println(i);
+        }
+    } // End of task
+
+    public static void task7() {
+        System.out.println("Задача 7");
+        for ( int i = 1; i <= 512; i *= 2 ) {
             System.out.println(i);
         }
     } // End of task
