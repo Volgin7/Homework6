4b5be84cba6663765c759e83c18dbb05e312c3f1
Volgin7 <8612020@gmail.com>
2023-02-27 12:26:34 +0300

    +task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
     }
-}
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        for ( int i = 1; i<=10; i++ ) {
+            System.out.println(i);
+        }
+    } // End of task
+
+}  // End of main
